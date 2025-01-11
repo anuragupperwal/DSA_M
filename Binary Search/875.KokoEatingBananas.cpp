58c32f9003a041b88fa5303f6fa10e8248f72ec0
Anurag  Upperwal <anuragupperwal@Anurags-MacBook-Pro-72.local>
2025-01-11 22:53:26 +0530

    LC875

--- Binary Search/875.KokoEatingBananas.cpp
+++ Binary Search/875.KokoEatingBananas.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int n = piles.size();
-
         int left = 1, right=INT_MIN;
         for(int i=0; i<n; i++) right = max(right, piles[i]);
 
@@ -14,7 +13,7 @@ public:
                 hours+=(piles[i]/mid);
                 hours += (piles[i]%mid > 0)? 1:0;
             }
-            
+
             if(hours <= h) right = mid;
             else left = mid+1;
         }
